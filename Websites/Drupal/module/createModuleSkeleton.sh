8f0075504030fded4fe72b7bc54ccd5f67bb3e61
thsmcoding <hassia.tamimou@gmail.com>
2021-06-24 23:11:53 +0200

    API: creating skeleton folder for DRupal module

--- Websites/Drupal/module/createModuleSkeleton.sh
+++ Websites/Drupal/module/createModuleSkeleton.sh
@@ -1,52 +1,79 @@
-#!bin/bash
-function touchBis() {
-	[[ "${1}" ]] || echo "Missing file path"; return
-	mkdir -p "$(dirname "$1")" && touch $1
+#!/bin/bash
+#INPUT : ABSOLUTE PATH FOR A FILE TO CREATE
+function createtouch() {
+	[[ "${1}" ]] || echo "Function createtouch : Missing file path"; return
+	#mkdir -p "$(dirname "$1")" && touch $1
+	echo "${1}"
 }
-#Set up the necessary components for a custom module
-function setModule() {
-	[ "${1}" ] || echo "You are missing a module name."; return
-	local moduleName= $1;
-	echo "Where do you want to set your module ?"
-	read "Do you wish to set up in current location?" yn
-	if [[ $yn =~[Yy]$ ]] ; then
-		local correct_path = ${pwd}
-		crMod	
+#CREATE THE BASIC DEFINITION FILES FOR A DRUPAL MODULE
+#module.info.yml
+#module.install
+#module.links.menu.yml etc...
+function drupalmoduleFiles() {
+    [[ "{1}" ]] || echo "Function drupalmoduleFiles: Missing module name!"; return
+    files=("README.md" ".info.yml" ".install" ".links.menu.yml" ".module" ".permissions.yml" ".routing.yml")
+    echo "Start creating the basic files for the module..."
+    for keys in "${!files[@]}"; do
+		if [ keys -eq "0" ]; then
+		    touch "${files[$keys]}";
+		else
+		    touch "${1}${files[$keys]}";
+		fi
+    done
+echo "Function drupalmoduleFiles: Main files for Drupal module have been created. Completed.";
+}
+#SET UP THE COMPONENTS FOR A CUSTOM MODULE
+function setupModule() {
+    [[ "${1}" ]] || echo "Function setupModule : You are missing a module name.";
+    local moduleName=$1;
+	echo "Module settings :"
+	read -p "Do you wish to set up in current location?" yn
+	if [[ $yn =~ ^[Yy]$ ]] ; then
+		local current_path=${pwd}
+		buildfolderMod	$current_path $moduleName
 	else
 		read -p "Choose a location for your module:" mod_path
-		crMod $mod_path
+		buildfolderMod $mod_path $moduleName
 	fi
 }
-function strmod() {
-	[[ "${1}" ]] || echo "You are missing a module name."; return
-	local mod_name = $1
+#CREATE ALL THE SUBFOLDERS AND FILES WITHIN THE ROOT FOLDER MODULE
+function createFolders() {
+	[[ "${1}" ]] || echo "Function createFolders: You are missing a module name.";
+	mod_name=$1
+	drupalmoduleFiles "${mod_name,,}" #to lowercase
 	folders=("config" "src" "templates")
-	for folder in ${folders[@]}; do
+	for folder in "${folders[*]}"; do
 		mkdir $folder
 		case $folder in
-			"config")
-				local settings = "./config/install/"+$mod_name+".settings.yml"
-				touchBis settings
-				local schema = "./config/schema/"+$mod_name+".schema.yml"
-				touchBis schema
+			"config" )
+			    settings = "./config/install/"${mod_name,,}".settings.yml"
+			    createtouch "${settings}"
+			    schema = "./config/schema/"${mod_name,,}".schema.yml"
+			    createtouch "${schema}"
 			;;
-			"src")
-				mkdir -p {./src/Controller, ./src/Form, ./src/Plugin, ./src/Plugin/Block, ./src/Test}
-
-			;;
-			"templates")
+			"src" )
+			    mkdir -p {./src/Controller, ./src/Form, ./src/Plugin, ./src/Plugin/Block, ./src/Tests};
+			    touch "./src/Controller/"${mod_name,,}"Controller.php";
+			    touch "./src/Form/"${mod_name,,}"BlockForm.php";
+			    touch "./src/Form/"${mod_name,,}"Form.php";
+			    touch "./src/Plugin/Block/"${mod_name,,}"Block.php";
+			    touch "./src/Tests/"${mod_name}"Tests.php";
+			    ;;
+			"templates" )
 				mkdir templates
-				local template= $mod_name+".html.twig"
+				template= ${mod_name,,}".html.twig"
 				cd ./templates; touch template
+				cd ..
 		esac
 	done
-
 }
-#Create older structure for a custom module in Drupal 
-function crMod() {
-	if [[ $# -eq 2 ]]  ; then
-	else
-		cd $1
-		strmod $2
-	fi
+#CREATE THE MAIN MODULE FOLDER STRUCTURE 
+function buildfolderMod() {
+    if [[ $# -eq 1 ]] ; then
+	echo "Function buildfolderMod: Starting to build module structure with module name as the only arg."
+	createFolders "${1}"
+    else
+	cd "${1}"
+	createFolders "${2}"
+    fi
 }
