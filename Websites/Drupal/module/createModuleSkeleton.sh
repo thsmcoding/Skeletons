9bc4feb13656be6da9d53f5c41ff5c5dd4e409f0
thsmcoding <hassia.tamimou@gmail.com>
2021-06-25 13:53:54 +0200

    API/FIX: setting up the folder structure for a custom module

--- Websites/Drupal/module/createModuleSkeleton.sh
+++ Websites/Drupal/module/createModuleSkeleton.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 #INPUT : ABSOLUTE PATH FOR A FILE TO CREATE
 function createtouch() {
-	[[ "${1}" ]] || echo "Function createtouch : Missing file path"; return
-	#mkdir -p "$(dirname "$1")" && touch $1
-	echo "${1}"
+    echo "NUMBER OF ARGS :"$#
+    [[ "${1}" ]] || echo "Function createtouch : Missing file path";
+    mkdir -p "$(dirname "$1")" && touch ${1}
+    echo ${1}
 }
 #CREATE THE BASIC DEFINITION FILES FOR A DRUPAL MODULE
 #module.info.yml
 #module.install
 #module.links.menu.yml etc...
 function drupalmoduleFiles() {
-    [[ "{1}" ]] || echo "Function drupalmoduleFiles: Missing module name!"; return
+    [[ "{1}" ]] || echo "Function drupalmoduleFiles: Missing module name!";
     files=("README.md" ".info.yml" ".install" ".links.menu.yml" ".module" ".permissions.yml" ".routing.yml")
     echo "Start creating the basic files for the module..."
     for keys in "${!files[@]}"; do
-		if [ keys -eq "0" ]; then
+		if [[ $keys -eq 0 ]]; then
 		    touch "${files[$keys]}";
 		else
 		    touch "${1}${files[$keys]}";
@@ -24,56 +25,62 @@ echo "Function drupalmoduleFiles: Main files for Drupal module have been created
 }
 #SET UP THE COMPONENTS FOR A CUSTOM MODULE
 function setupModule() {
-    [[ "${1}" ]] || echo "Function setupModule : You are missing a module name.";
-    local moduleName=$1;
-	echo "Module settings :"
-	read -p "Do you wish to set up in current location?" yn
-	if [[ $yn =~ ^[Yy]$ ]] ; then
-		local current_path=${pwd}
-		buildfolderMod	$current_path $moduleName
-	else
-		read -p "Choose a location for your module:" mod_path
-		buildfolderMod $mod_path $moduleName
-	fi
+    [[ "${1}" ]] || echo "Function setupModule : You are missing a module name."return;
+    moduleName=${1};
+    echo "Chosen module name : " $moduleName
+    echo "Module settings :"
+    read -p "Do you wish to set up in current location?" yn
+    if [[ $yn =~ ^[Yy]$ ]] ; then
+	local current_path=${pwd}
+	mkdir ./$moduleName && cd ./$moduleName
+	buildfolderMod $moduleName
+    else
+	read -p "Choose a location for your module:" mod_path
+	buildfolderMod $mod_path $moduleName
+    fi
 }
 #CREATE ALL THE SUBFOLDERS AND FILES WITHIN THE ROOT FOLDER MODULE
 function createFolders() {
-	[[ "${1}" ]] || echo "Function createFolders: You are missing a module name.";
-	mod_name=$1
-	drupalmoduleFiles "${mod_name,,}" #to lowercase
+	[[ "${1}" ]] || echo "Function createFolders: You are missing a module name."
+	mod_name=${1}
+	mod_name=${mod_name,,}
+	drupalmoduleFiles ${mod_name} #to lowercase
 	folders=("config" "src" "templates")
-	for folder in "${folders[*]}"; do
-		mkdir $folder
-		case $folder in
-			"config" )
-			    settings = "./config/install/"${mod_name,,}".settings.yml"
-			    createtouch "${settings}"
-			    schema = "./config/schema/"${mod_name,,}".schema.yml"
-			    createtouch "${schema}"
-			;;
-			"src" )
-			    mkdir -p {./src/Controller, ./src/Form, ./src/Plugin, ./src/Plugin/Block, ./src/Tests};
-			    touch "./src/Controller/"${mod_name,,}"Controller.php";
-			    touch "./src/Form/"${mod_name,,}"BlockForm.php";
-			    touch "./src/Form/"${mod_name,,}"Form.php";
-			    touch "./src/Plugin/Block/"${mod_name,,}"Block.php";
-			    touch "./src/Tests/"${mod_name}"Tests.php";
-			    ;;
-			"templates" )
-				mkdir templates
-				template= ${mod_name,,}".html.twig"
-				cd ./templates; touch template
-				cd ..
-		esac
+	for folder in "${folders[@]}"; do
+	    mkdir $folder;
+	    echo "createFolders:" "$folder" ;
+	    case "$folder" in
+		"config")
+		    settings="./config/install/"${mod_name}".settings.yml"
+		    createtouch ${settings}
+		    schema="./config/schema/"${mod_name}".schema.yml"
+		    createtouch ${schema}
+		    ;;
+		"src")
+		    mkdir -p {"./src/Controller","./src/Form","./src/Plugin","./src/Plugin/Block","./src/Tests"};
+		    touch "./src/Controller/"${mod_name}"Controller.php";
+		    touch "./src/Form/"${mod_name}"BlockForm.php";
+		    touch "./src/Form/"${mod_name}"Form.php";
+		    touch "./src/Plugin/Block/"${mod_name}"Block.php";
+		    touch "./src/Tests/"${mod_name}"Tests.php";
+		    ;;
+		"templates")
+		    template=${mod_name,,}".html.twig"
+		    cd ./templates && touch $template
+		    cd ..
+	    esac
 	done
 }
 #CREATE THE MAIN MODULE FOLDER STRUCTURE 
 function buildfolderMod() {
     if [[ $# -eq 1 ]] ; then
 	echo "Function buildfolderMod: Starting to build module structure with module name as the only arg."
-	createFolders "${1}"
+	createFolders ${1}
+	cd ..
     else
-	cd "${1}"
-	createFolders "${2}"
+	cd ${1}
+	mkdir ${2} && cd ${2}
+	createFolders ${2}
+	cd ..
     fi
 }
